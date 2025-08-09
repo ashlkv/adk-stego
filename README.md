8242d572d0cd8e9f22e1920364db3814a6334b7c
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2025-08-09 18:22:04 +0200

    Basic audio streaming agent

---
+++ README.md
@@ -0,0 +1,12 @@
+See https://google.github.io/adk-docs/streaming/custom-streaming
+
+## Launch
+
+Use venv. (Does not run with uv).
+
+```bash
+python -m venv .venv
+
+# Activate (each new terminal)
+source .venv/bin/activate
+```
---
+++ google_search_agent/agent.py
@@ -0,0 +1,52 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Optional
+
+from google.adk.agents import Agent
+from google.adk.tools import google_search  # Import the tool
+from google.adk.agents.callback_context import CallbackContext
+from google.adk.models import LlmResponse, LlmRequest
+
+
+def before_model_callback(
+        callback_context: CallbackContext, llm_request: LlmRequest
+) -> Optional[LlmResponse]:
+   print("Before model callback")
+
+
+def after_model_callback(callback_context: CallbackContext, llm_response: LlmResponse) -> None:
+   print("After model callback")
+   return None
+
+
+def after_agent_callback() -> None:
+   print("after_agent_callback")
+   return None
+
+root_agent = Agent(
+   # A unique name for the agent.
+   name="google_search_agent",
+   # The Large Language Model (LLM) that agent will use.
+   model="gemini-2.0-flash-exp", # if this model does not work, try below
+   #model="gemini-2.0-flash-live-001",
+   # A short description of the agent's purpose.
+   description="Agent to answer questions using Google Search.",
+   # Instructions to set the agent's behavior.
+   instruction="Answer the question using the Google Search tool.",
+   # Add google_search tool to perform grounding with Google search.
+   tools=[google_search],
+   after_model_callback=after_model_callback,
+   before_model_callback=before_model_callback,
+   after_agent_callback=after_agent_callback
+)
